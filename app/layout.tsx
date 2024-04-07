c3f9e2be78799622119b8aa5a2dda23c54719041
Hoai Tran <tranngochoai02051999@gmail.com>
2024-04-07 20:15:03 +0700

    [FEATURE] Implement Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <Providers>{children}</Providers>
       </body>
     </html>
